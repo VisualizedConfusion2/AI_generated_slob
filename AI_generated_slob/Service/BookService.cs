90652f708d378ea243c85cbfa86891c9592056e2
T04 <thh005@edu.zealand.dk>
2025-09-30 09:44:30 +0200

    oprettelse af basis klasser og mapper

---
+++ AI_generated_slob/Service/BookService.cs
@@ -0,0 +1,34 @@
+﻿using AI_generated_slob.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_generated_slob.Service
+{
+    public class BookService
+    {
+        private readonly List<Book> _books;
+
+        public BookService()
+        {
+            _books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Clean Code", Author = "Robert C. Martin", Price = 299 },
+                new Book { Id = 2, Title = "The Pragmatic Programmer", Author = "Andrew Hunt", Price = 349 },
+                new Book { Id = 3, Title = "Design Patterns", Author = "Erich Gamma", Price = 399 }
+            };
+        }
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _books;
+        }
+
+        public Book? GetBookById(int id)
+        {
+            return _books.FirstOrDefault(b => b.Id == id);
+        }
+    }
+}
