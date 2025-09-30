b4180620fac8a36996fec8e64d2217418849ba78
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-09-30 10:06:25 +0200

    Did a thing

---
+++ AI_generated_slob/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_generated_slob.Models
+{
+    public enum UserTypes { Costumer, Moderator, Admin }
+    internal class User
+    {
+        private int UserID { get; set; }
+        public UserTypes UserType { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Address UAdress { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ AI_generated_slob/Models/address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_generated_slob.Models
+{
+    internal class Address
+    {
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string AddressLine { get; set; }
+        public string Floor { get; set; }
+        public string ApartmentNumber { get; set; }
+
+
+    }
+}
